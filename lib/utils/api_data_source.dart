98e75c526b4d50cf6f9d66f3dd48f1ad887a1256
auditoosaputra <auditoonny25@gmail.com>
2024-05-29 22:03:22 +0700

    feat: update api

--- lib/utils/api_data_source.dart
+++ lib/utils/api_data_source.dart
@@ -2,7 +2,11 @@ import 'base_network.dart';
 
 class ApiDataSource {
   static ApiDataSource instance = ApiDataSource();
-  Future<Map<String, dynamic>> loadMember() {
+  Future<Map<String, dynamic>> loadData() {
     return BaseNetwork.get("jkt48_member.json");
+  }
+
+  Future<Map<String, dynamic>> loadEvent() {
+    return BaseNetwork.get("jkt48_event.json");
   }
 }
