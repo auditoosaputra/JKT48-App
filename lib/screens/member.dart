b5d634044baee934d4b34cf9638472e06c1a96b8
auditoosaputra <auditoonny25@gmail.com>
2024-05-30 22:06:39 +0700

    feat: add search functionr

--- lib/screens/member.dart
+++ lib/screens/member.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jkt48_app/model/jkt48_model.dart';
+import 'package:jkt48_app/model/member_model.dart';
 import 'package:jkt48_app/screens/member_detail.dart';
 import 'package:jkt48_app/utils/api_data_source.dart';
 
@@ -10,9 +10,11 @@ class MemberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Members> filteredMembers;
+
     return Container(
       child: FutureBuilder(
-        future: ApiDataSource.instance.loadMember(),
+        future: ApiDataSource.instance.loadData(),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.hasError) {
             return Center(child: Text("Error: ${snapshot.error}"));
@@ -23,10 +25,14 @@ class MemberPage extends StatelessWidget {
             );
           }
           if (snapshot.hasData) {
-            DataMember member = DataMember.fromJson(snapshot.data);
-            var filteredMembers = member.members?.where((m) {
+            DataJKT48 member = DataJKT48.fromJson(snapshot.data);
+            filteredMembers = member.members!.where((m) {
               return m.name!.toLowerCase().contains(searchQuery.toLowerCase());
             }).toList();
+
+            if (filteredMembers.isEmpty) {
+              return _noDataFound();
+            }
 
             return GridView.builder(
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -37,10 +43,10 @@ class MemberPage extends StatelessWidget {
               ),
               itemCount: filteredMembers?.length ?? 0,
               itemBuilder: (BuildContext context, int index) {
-                final memberData = filteredMembers?[index];
+                final memberData = filteredMembers![index];
                 return InkWell(
                   onTap: () {
-                    Navigator.push(
+                    Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
                         builder: (context) =>
@@ -99,6 +105,12 @@ class MemberPage extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  Widget _noDataFound() {
+    return Center(
+      child: Text("No Data Available"),
     );
   }
 }
