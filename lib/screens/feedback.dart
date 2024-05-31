ce961fc10144bfb00b248873893f8a7219606313
auditoosaputra <auditoonny25@gmail.com>
2024-05-31 22:08:02 +0700

    feat: change to feedback page

---
+++ lib/screens/feedback.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FeedbackPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Kesan dan Saran"),
+      ),
+    );
+  }
+}
