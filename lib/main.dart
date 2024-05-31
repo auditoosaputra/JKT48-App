fbcf114549f6585460e5884bf8aa51df81333789
auditoosaputra <auditoonny25@gmail.com>
2024-05-31 22:08:31 +0700

    feat: add database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:jkt48_app/model/member_model.dart';
+import 'package:jkt48_app/model/user.dart';
 import 'package:jkt48_app/screens/home.dart';
+import 'package:jkt48_app/screens/login.dart';
+import 'package:jkt48_app/screens/register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  // Hive.registerAdapter(MembersAdapter());
+  Hive.registerAdapter(UserAdapter());
+  // Hive.registerAdapter(MembersAdapter());
+  Hive.registerAdapter(EventsAdapter());
+
   runApp(const MainApp());
 }
 
@@ -10,6 +23,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
